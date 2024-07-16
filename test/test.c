1faaab29253de1f048322e0dacca148895763f78
Beckett Avary <beckettav@gmail.com>
2024-07-16 11:30:47 -0700

    improved test ergonomics and created basic string test case. unit testing pending

--- test/test.c
+++ test/test.c
@@ -5,32 +5,18 @@
 
 #include <marble_types.h>
 #include <marble_list.h>
+#include <string.h>
 
-//void print_list(void* data) {
-//    u32 v = *((u32*)data);
-//    printf("%"PRIu32"\n", v);
-//}
-
-void print_list(void* data) {
+void u32_print(void* data) {
     u32 v = *((u32*) data);
     printf("%"PRIu32"\n", v);
-}
-
-void u32_free(void* in) {
-    free(in);
-}
-
-void* u32_copy(void* in) {
-    u32* out = malloc(sizeof (*out));
-    *out = *(u32*)in;
-    return out;
 }
 
 bool u32_filter(void* in) {
     return (*(u32*) in) < 3;
 }
 
-int main(void) {
+int u32_main(void) {
 
     mlist_cfg cfg = {0};
     mlist list = mlist_init(cfg);
@@ -44,17 +30,65 @@ int main(void) {
     mlist_push_back(list, &c);
 
 
-    mlist_map(list, print_list);
+    mlist_map(list, u32_print);
 
     mlist new_list = mlist_filter(list, u32_filter);
 
-    mlist_map(new_list, print_list);
+    mlist_map(new_list, u32_print);
 
     mlist_free(list);
 
-    mlist_map(new_list, print_list);
+    mlist_map(new_list, u32_print);
     mlist_free(new_list);
     
     return 0;
+}
+
+void str_print(void* data) {
+    printf("%s\n", (char*) data);
+}
+
+void* str_copy(void* data) {
+    size_t sz = strlen((char*) data);
+    char* out = calloc(sz+1, sizeof (char));
+    strcpy(out, data);
+    return out;
+}
+
+void str_free(void* data) {
+    free(data);
+}
+
+void* str_reduce(void* d1, void* d2) {
+    size_t sz = strlen(d1) + strlen(d2);
+    char* out = calloc(sz+1, sizeof (char));
+    strcat(out, d1);
+    strcat(out, d2);
+    return out;
+}
+
+//int str_main(void) {
+int main(void) {
+
+    mlist_cfg cfg = {
+        .copy = str_copy,
+        .free = str_free
+    };
+    mlist list = mlist_init(cfg);
+
+    mlist_push_back(list, "test0");
+    mlist_push_back(list, "test1");
+    mlist_push_back(list, "test2");
+
+    mlist_map(list, str_print);
+    printf("---\n");
+
+    char* out = mlist_reduce(list, str_reduce);
+
+    printf("%s\n", out);
+
+    free(out);
+
+    mlist_free(list);
 }
 
