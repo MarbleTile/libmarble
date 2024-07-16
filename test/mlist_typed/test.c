0361be4552fccca401f64a9400e813ccad043dce
Beckett Avary <beckettav@gmail.com>
2024-07-16 15:31:34 -0700

    trivial `mlist_typed` test

--- test/mlist_typed/test.c
+++ test/mlist_typed/test.c
@@ -1,9 +1,31 @@
 
 #include <stdio.h>
 
+#include <marble/types.h>
+
+#define MLIST_TYPE u32
+#include <marble/list_typed.h>
+
+void u32_print(u32 data) {
+    printf("%u\n", data);
+}
+
 int main(void) {
 
+    mlist_cfg_u32 cfg = {0};
 
+    mlist_u32 list = mlist_init_u32(cfg);
+
+    mlist_push_back_u32(list, 1);
+    mlist_push_back_u32(list, 2);
+    mlist_push_back_u32(list, 3);
+    mlist_push_back_u32(list, 4);
+    mlist_push_back_u32(list, 5);
+
+    mlist_map_u32(list, u32_print);
+    printf("---\n");
+
+    mlist_free_u32(list);
 
     return 0;
 }
